a93e9a55a2f1807497690f9a855fe006c6aba24d
Stacie Taylor-Cima <stacietaylor-cima@ip-172-27-252-113.ec2.internal>
2019-06-23 12:37:15 -0700

    add books to firestore

--- src/components/book/AddBook.js
+++ src/components/book/AddBook.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { withFirestore } from "react-firestore";
 
-// Status Report: I've got the state logging to the console on click.
-// Now I need to find a way to wrap this component in the higher order component, withFirestore, so that I can add new book to the db.
-// Andrew has this in App componenet in BYOHS. react-firestore docs have an example that I've included below.
+// Status Report: I've got data sending to firestore from the addBook method!
 
 class AddBook extends Component {
   state = {
@@ -14,6 +12,21 @@ class AddBook extends Component {
     synopsis: ""
   };
 
+  addBook = s => {
+    let { firestore } = this.props;
+
+    firestore
+      .collection('books')
+      .add({
+        title: s.title,
+        author: s.author,
+        bookURL: s.bookURL,
+        imageURL: s.imageURL,
+        synopsis: s.synopsis
+    })
+    console.log("addBook function is running");
+  };
+
   handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
@@ -21,9 +34,8 @@ class AddBook extends Component {
   };
 
   handleSubmit = e => {
-    console.log(this.state);
     e.preventDefault();
-    this.props.addBook(this.state);
+    this.addBook(this.state);
   };
 
   render() {
@@ -63,35 +75,5 @@ class AddBook extends Component {
     );
   }
 }
-
-// Using react-firestore's code:
-// class AddBook extends Component {
-//     state = {
-//       title: null,
-//     };
-
-//     componentDidMount() {
-//       const { firestore } = this.props;
-
-//       firestore.doc('books/1').onSnapshot(snapshot => {
-//         this.setState({ books: snapshot });
-//       });
-//     }
-
-//     render() {
-//       const { book } = this.state;
-//       const bookData = book ? book.data() : null;
-
-//       return (
-//         <div>
-//           <h1>{bookData.title}</h1>
-//           <h2>
-//             {bookData.author} - {bookData.bookURL}
-//           </h2>
-//           <p>{bookData.synopsis}</p>
-//         </div>
-//       )
-//     }
-//   }
 
 export default withFirestore(AddBook);
